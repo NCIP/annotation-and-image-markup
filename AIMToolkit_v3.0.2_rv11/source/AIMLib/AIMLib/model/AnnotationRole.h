c5a8c6a7ebb76651d150dec244d99916dc25caae
Yogesh Prasai <yogesh.prasai@nih.gov>
2013-06-05 11:02:07 -0400

    Copyright header re-writed

--- AIMToolkit_v3.0.2_rv11/source/AIMLib/AIMLib/model/AnnotationRole.h
+++ AIMToolkit_v3.0.2_rv11/source/AIMLib/AIMLib/model/AnnotationRole.h
@@ -1,7 +1,7 @@
 /*
   Classname: AnnotationRole
 
-*  Copyright Northwestern University
+*  2007 – 2013 Copyright Northwestern University
 *
 *  Distributed under the OSI-approved BSD 3-Clause License.
 *  See http://ncip.github.com/annotation-and-image-markup/LICENSE.txt for details.
